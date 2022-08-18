2868d1dd33b99a047109d697214ce3e28f46cb75
tamerlanstt <tamerlan.stt@gmail.com>
2022-08-18 23:20:52 +0600

    salary

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ app.use('/', express.static(path.join(__dirname + '/public')));
 app.post('/api/post', (req, res) => {
   const b = req.body
   connection.query(`INSERT INTO oper (title, amount, comment, category, date, addedAt) VALUES ("${b.title}", ${b.amount}, "${b.comment}", ${b.category}, "${b.date}", NOW())`, function (err, result) {
-    console.log('add: done!')
+    console.log('add(o): done!')
   });
   res.redirect('/');
 })
@@ -61,9 +61,17 @@ app.post('/api/post', (req, res) => {
 app.post('/api/edit', (req, res) => {
   const b = req.body
   connection.query(`UPDATE oper SET title="${b.title}", amount=${b.amount}, comment="${b.comment}", category=${b.category}, date="${b.date}" WHERE ID=${b.id}`, function (err, result) {
-    console.log('edit: done!')
+    console.log('edit(o): done!')
   });
   res.redirect('/');
+})
+
+app.post('/api/reg', (req, res) => {
+  const b = req.body
+  connection.query(`INSERT INTO work (amount, date, rate, addedAt) VALUES (${b.amount}, "${b.date}", ${b.rate}, NOW())`, function (err, result) {
+    console.log('add(w): done!')
+  });
+  res.redirect('/reg.html');
 })
 
 app.get('/api/info/:id', (req, res) => {
@@ -74,13 +82,34 @@ app.get('/api/info/:id', (req, res) => {
 
 app.get('/api/remove/:id', (req, res) => {
   connection.query(`DELETE FROM oper WHERE oper.id = ${req.params.id}`, function (err, result) {
-    console.log('remove: done!')
+    console.log('remove(o): done!')
   });
   res.redirect('/');
 })
 
+app.get('/api/reg/remove/:id', (req, res) => {
+  connection.query(`DELETE FROM work WHERE work.id = ${req.params.id}`, function (err, result) {
+    console.log('remove(w): done!')
+  });
+  res.redirect('/reg.html');
+})
+
+app.get('/api/reg/clear', (req, res) => {
+  connection.query(`DELETE FROM work`, function (err, result) {
+    console.log('remove(w+): done!')
+    res.send(result);
+  });
+  res.redirect('/reg.html');
+})
+
 app.get('/api/load', (req, res) => {
   connection.query("SELECT * FROM oper ORDER BY date DESC, addedAt DESC, id DESC", function (err, result) {
+    res.send(result);
+  });
+})
+
+app.get('/api/load/reg', (req, res) => {
+  connection.query("SELECT * FROM work ORDER BY date DESC, addedAt DESC, id DESC", function (err, result) {
     res.send(result);
   });
 })
--- public/main.js
+++ public/main.js
@@ -8,21 +8,21 @@ function changeCA() {
   el.innerHTML = `<i class="${c[parseInt(se.value)]} fa-fw"></i>`
 }
 
-function dotreplace() {
-  var tmp = document.getElementById('amount')
+function dotreplace(e) {
+  var tmp = document.getElementById(e.target.id)
   tmp.value = tmp.value.replace(/,/g, '.')
 }
 
 function deci_check(e) {
   e = (e ? e : window.event)
-  var val = document.getElementById('amount').value
+  var val = document.getElementById(e.target.id).value
   var ind = val.substring(0, e.target.selectionStart) + e.key + val.substring(e.target.selectionStart, val.length);
   return ['0','1','2','3','4','5','6','7','8','9','0','-','.',','].includes(e.key) && ((ind.match(/^(-|)[0-9]+(\.|\,){0,1}[0-9]{0,2}$/gi) || []).length == 1)
 }
 
 function check_paste(e) {
   e = (e ? e : window.event)
-  var elm = document.getElementById('amount')
+  var elm = document.getElementById(e.target.id)
   var ind = elm.value.substring(0, e.target.selectionStart) + e.clipboardData.getData('Text') + elm.value.substring(e.target.selectionStart, elm.value.length);
   return ((ind.match(/^(-|)[0-9]+(\.|\,){0,1}[0-9]{0,2}$/gi) || []).length == 1)
 }
